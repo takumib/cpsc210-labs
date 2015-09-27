b33b5ead3b0bf274c77ac38a7cc6449a156965b7
Takumi Bolte <takumib@gmail.com>
2015-09-27 17:12:04 -0400

    adding tests to main

--- labs/lab6/src/main.c
+++ labs/lab6/src/main.c
@@ -1,45 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 #include "list.h"
 #include "iterator.h"
 
 void list_create_test();
+void list_append_test();
+void list_remove_first_test();
+void printGreen(char*);
+void printRed(char*);
 
 int main()
 {
     list_create_test();
-    /*
-    list* l = list_create(); 
-
-    int data[4] = {0, 1, 2, 3};
-
-    for(int i = 0; i < 4; i++) {
-        list_append(l, &data[i]);
-    }
-
-    iterator* iter = iter_create(l);
-
-    void* n = iter_next(iter);
-    printf("%d, ", *((int*)n));
-    
-    iter_remove(iter);
-    n = iter_next(iter);
-    printf("%d, ", *((int*)n));
-    
-    printf("\n");
-
-    iter = iter_create(l);
-    n = iter_next(iter);
-    printf("%d, ", *((int*)n));
-    n = iter_next(iter);
-    printf("%d, ", *((int*)n));
-    printf("\n");
-
-    printf("\n");
-    */
+    list_append_test();
+    list_remove_first_test();
 
     return 0;
 }
@@ -49,8 +26,64 @@ void list_create_test()
     list* list = list_create();
     if(list != NULL) {
         free(list);
-        printf("[list_create_test: PASSED]\n");
+        printf("[list_create_test: ");
+        printGreen("PASSED");
+        printf("]\n");
     } else {
         printf("list_create_test: FAILED]\n");
+        printRed("FAILED");
+        printf("]\n");
     }
+}
+
+void list_append_test()
+{
+    list* l = list_create();
+    int x = 1;
+    
+    list_append(l, &x);
+
+    if(l->head != NULL) {
+       printf("[list_append_test1: "); 
+       printGreen("PASSED");
+       printf("]\n");
+    } else {
+        printf("[list_append_test1: ");
+        printRed("FAILED");
+        printf("]\n");
+        exit(-1);
+    }
+
+    free(l->head);
+    free(l);
+}
+
+void list_remove_first_test()
+{
+    list* l = list_create();
+    int x = 0;
+
+    list_append(l, &x);
+    list_remove_first(l);
+
+    if(l->head == NULL) {
+        printf("[list_remove_first_test1: "); 
+        printGreen("PASSED");
+        printf("]\n");
+    } else {
+        printf("[list_remove_first_test1: ");
+        printRed("FAILED");
+        printf("]\n");
+        exit(-1);
+    }
+}
+
+void printGreen(char* msg)
+{
+    printf("\x1b[1m\x1b[32m%s\x1b[0m", msg);
+}
+
+void printRed(char* msg)
+{
+    printf("\x1b[1m\x1b[31m%s\x1b[0m", msg);
 }
