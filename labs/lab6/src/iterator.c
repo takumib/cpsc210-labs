973c388c9281db2c982848fe4c88f97c84b4089a
Takumi Bolte <takumib@gmail.com>
2015-09-27 15:07:07 -0400

    added src directory

---
+++ labs/lab6/src/iterator.c
@@ -0,0 +1,31 @@
+#include "iterator.h"
+
+#include <stdlib.h>
+
+iterator* iter_create(list* l)
+{
+}
+
+void iter_advance(iterator* iter)
+{
+}
+
+void* iter_next(iterator* iter)
+{
+}
+
+void* iter_prev(iterator* iter)
+{
+}
+
+int iter_at_end(iterator* iter)
+{
+}
+
+node* iter_get(iterator* iter)
+{
+}
+
+void iter_remove(iterator* iter)
+{
+}
---
+++ labs/lab6/src/list.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+#include <stdlib.h>
+
+list* list_create()
+{
+}
+
+void list_append(list* l, void* item)
+{
+}
+
+void list_removeLast(list* l)
+{
+}
+
+void list_removeFirst(list* l)
+{
+}
+
+void list_copy(list* l, list* out)
+{
+}
